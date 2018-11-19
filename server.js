0ff7c9b5b1c2c649a0fae66b5841f57d092ab0fb
wanggege919 <1914161290@qq.com>
2018-11-19 22:35:49 +0800

    使用image发请求

--- server.js
+++ server.js
@@ -52,14 +52,17 @@ var server = http.createServer(function(request, response){
    `)
 
     response.end()
-  }else if(path === '/pay' && method.toUpperCase() === 'POST' ){
+  }else if(path === '/pay'){
     var amount = fs.readFileSync('./db', 'utf8')
     var newAmount = amount - 1
     if(Math.random()>0.5){
       fs.writeFileSync('./db', newAmount)
-      response.write('success')
-     
+      response.setHeader('Content-Type','image/jpg')
+      response.statusCode = 200
+      response.write(fs.readFileSync('./cat.jpg'))
+    
     }else{
+      response.statusCode = 400
       response.write('fail')    
     }
      response.end()
